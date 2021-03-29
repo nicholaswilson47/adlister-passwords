51dbca5a7f2c262ed2fd88d11cc06830565bd50f
Nicholas Wilson <nicholas.a.wilson47@gmail.com>
2021-03-29 05:28:31 -0500

    don't need category table

--- migration.sql
+++ migration.sql
@@ -1,8 +1,6 @@
 USE adlister_db;
 DROP TABLE IF EXISTS ads;
 DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS categories;
-DROP TABLE IF EXISTS ads_categories;
 CREATE TABLE users (
                        id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
                        username VARCHAR(240) NOT NULL UNIQUE,
