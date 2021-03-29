0697525ba2c1a4fc20b8111be781fd8568b3e53f
Nicholas Wilson <nicholas.a.wilson47@gmail.com>
2021-03-29 03:26:53 -0500

    fixed previous issue site loads

--- src/main/java/com/codeup/adlister/dao/Ads.java
+++ src/main/java/com/codeup/adlister/dao/Ads.java
@@ -10,10 +10,18 @@ public interface Ads {
     // insert a new ad and return the new ad's id
     Long insert(Ad ad);
 
+    Ad individualAd(Long id);
+
+    void updateProfile(String username, String password, String email, long id);
+
     List<Ad> getAdsByUser(long id);
 
     List<Ad> deleteAd(long id);
 
     List<Ad> updateAd(long id,String title,String description);
+
+    List<Ad> search(String term);
+
+    Ad byAdID(long id);
 
 }
