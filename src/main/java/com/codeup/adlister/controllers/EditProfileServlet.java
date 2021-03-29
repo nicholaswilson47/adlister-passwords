ab46aee4a375c614c29c6666e9bb94ce857d4dbf
Nicholas Wilson <nicholas.a.wilson47@gmail.com>
2021-03-28 22:19:24 -0500

    EditProfileServlet

--- src/main/java/com/codeup/adlister/controllers/EditProfileServlet.java
+++ src/main/java/com/codeup/adlister/controllers/EditProfileServlet.java
@@ -1,5 +1,8 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,6 +16,16 @@ public class EditProfileServlet extends HttpServlet {
         request.getRequestDispatcher("/WEB-INF/edit-profile.jsp") .forward(request, response);
     }
 
-    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String newPassword = request.getParameter("newPassword");
+        User user = (User) request.getSession().getAttribute("user");
+        long userId =user.getId();
+        System.out.println(username + email + newPassword + userId);
+        DaoFactory.getUsersDao().updateProfile(username, email, newPassword, userId);
+        response.sendRedirect("/profile");
+    }
 
 }
